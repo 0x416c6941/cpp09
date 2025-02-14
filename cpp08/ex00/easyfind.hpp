ab1617a3e9e7131a909f168c04f8692e15ea48b2
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-02-14 15:08:44 +0100

    cpp08/ex00: Placebo

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// "The first one has type T".
+// Well, then we can also just set the type to "T &", right?
 template <typename T>
 typename T::iterator easyfind(T & container, int value);
 
