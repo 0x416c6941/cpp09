6c4cc5580bebc5eaf7687b366a1252e25c76ed06
Asset Sagymbayev <69522068+0x416c6941@users.noreply.github.com>
2025-02-28 08:46:52 +0100

    cpp07/ex00: Placebo

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -7,11 +7,11 @@ int main() {
 
     ::swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
     return 0;
 }
